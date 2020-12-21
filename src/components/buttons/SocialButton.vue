1d35447fcdd7239f048b56897efefb54ecf8a73f
Alexey Salangin <a7ls@ya.ru>
2020-12-21 04:25:16 +0300

    Landing. Top bar

---
+++ src/components/buttons/SocialButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button">
+    <slot/>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background: #BDBDBD;
+  border: none;
+  border-radius: 50%;
+  transition: 0.3s ease-in-out all;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 36px;
+  height: 36px;
+}
+.button:hover {
+  background: darkgray;
+}
+.button:active {
+  background: gray;
+}
+button:focus {
+  outline: 0;
+}
+.button:after {
+  width: 18px;
+  height: 18px;
+}
+</style>
--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,32 +1,65 @@
 <template>
   <div>
-    <div class="landing-top-bar">
-      <div class="landing-logo">
-        <img src="@/assets/logo.svg">
-      </div>
-      <div class="landing-social-buttons">
-        <div>
-          icon
+    <div>
+      <div class="landing-top-bar">
+        <div class="landing-logo">
+          <img src="@/assets/logo.svg">
+        </div>
+        <div class="landing-social-buttons">
+          <social-button class="button">
+            <icon-base icon-name="twitter">
+              <icon-twitter/>
+            </icon-base>
+          </social-button>
+          <social-button class="button">
+            <icon-base icon-name="google">
+              <icon-google/>
+            </icon-base>
+          </social-button>
+          <social-button class="button">
+            <icon-base icon-name="instagram">
+              <icon-instagram/>
+            </icon-base>
+          </social-button>
+          <social-button class="button">
+            <icon-base icon-name="vk">
+              <icon-vk/>
+            </icon-base>
+          </social-button>
         </div>
       </div>
-    </div>
-    <div>
-      First section
-    </div>
-    <div>
-      Second section
-    </div>
-    <div>
-      Third section
+      <div>
+        First section
+      </div>
+      <div>
+        Second section
+      </div>
+      <div>
+        Third section
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Landing'
-}
+import IconBase from '@/components/Icons/IconBase'
+import SocialButton from '@/components/buttons/SocialButton'
+import IconTwitter from '@/components/Icons/IconTwitter'
+import IconInstagram from '@/components/Icons/IconInstagram'
+import IconGoogle from '@/components/Icons/IconGoogle'
+import IconVk from '@/components/Icons/IconVK'
 
+export default {
+  name: 'Landing',
+  components: {
+    SocialButton,
+    IconBase,
+    IconTwitter,
+    IconInstagram,
+    IconGoogle,
+    IconVk
+  }
+}
 </script>
 
 <style scoped>
@@ -38,12 +71,18 @@ export default {
     height: 48pt;
   }
   .landing-logo {
+    display: flex;
     background-color: yellow;
+    align-items: center;
   }
   .landing-social-buttons {
     display: flex;
-    align-items: flex-end;
+    align-items: center;
     align-content: flex-end;
+    flex-wrap: nowrap;
     background-color: antiquewhite;
+  }
+  .landing-social-buttons button:not(:first-of-type) {
+    margin-left: 40px;
   }
 </style>
