094c8f9d28291c66f9e1c09e7fce212f20a3ec10
Alexey Salangin <a7ls@ya.ru>
2021-01-04 21:25:21 +0300

    Create side menu

--- src/pages/Home/Home.ts
+++ src/pages/Home/Home.ts
@@ -1,5 +1,29 @@
 import LandingStats from '@/components/LandingStats/LandingStats.vue'
+import MenuOverlay from '@/components/MenuOverlay.vue'
 
-export default {
-  components: { LandingStats }
-}
+import Vue from 'vue'
+import { bus } from '@/main'
+
+export default Vue.extend({
+  components: {
+    LandingStats,
+    MenuOverlay
+  },
+  data () {
+    return {
+      menuVisible: false,
+      links: [
+        { title: 'Главная', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', target: '_black' },
+        { title: 'Профиль', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', target: '_black' },
+        { title: 'Задания', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', target: '_black' },
+        { title: 'Магазин', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', target: '_black' }
+      ],
+      bus: new Vue()
+    }
+  },
+  methods: {
+    openMenu () {
+      bus.$emit('openMenuEventName')
+    }
+  }
+})
