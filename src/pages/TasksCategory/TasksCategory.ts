e33ab53fbcffbde0d19825bd8cf1624f0b6df5fe
Alexey Salangin <a7ls@ya.ru>
2021-01-07 20:04:51 +0300

    Get tasks from server

--- src/pages/TasksCategory/TasksCategory.ts
+++ src/pages/TasksCategory/TasksCategory.ts
@@ -1,12 +1,38 @@
 import Vue from 'vue'
+import * as rm from 'typed-rest-client/RestClient'
+
+interface Task {
+  name: string;
+}
+
+interface TaskRecord {
+  id: string;
+  fields: Array<Task>;
+}
+
+interface TaskList {
+  records: Array<TaskRecord>;
+}
 
 export default Vue.extend({
   components: {
   },
   data () {
     return {
+      tasks: new Array<Task>()
     }
   },
+  mounted () {
+    this.fetchTasks().then(tasks => {
+      this.tasks = tasks
+    })
+  },
   methods: {
+    fetchTasks: async () => {
+      const rest: rm.RestClient = new rm.RestClient('rest', 'https://api.airtable.com/v0/appL06XW0QrDbpxxT/')
+      const response: rm.IRestResponse<TaskList> = await rest.get<TaskList>('Tasks?api_key=keyUHl2tkCf4DEGqC')
+      const tasks: Array<Task> = response?.result?.records.flatMap(x => x.fields) ?? []
+      return tasks
+    }
   }
 })
