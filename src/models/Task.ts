b86aeeeb5233c56fb8f6031a10f0491fc90187e2
Alexey Salangin <a7ls@ya.ru>
2021-01-08 04:12:26 +0300

    Show localized task date

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,3 +1,5 @@
+import { format } from 'ts-date/locale/ru'
+
 export enum TaskType {
   Street = 1,
   Home,
@@ -12,22 +14,32 @@ interface TaskAttachment {
 export interface TaskInterface {
   name: string;
   description: string;
+  date: string;
   attachments: Array<TaskAttachment>;
   image: string;
+  dateDescription: string;
 }
 
 export class Task implements TaskInterface {
   name: string;
   description: string;
+  date: string;
   attachments: Array<TaskAttachment>;
 
   constructor (task: TaskInterface) {
     this.name = task.name
     this.description = task.description
+    this.date = task.date
     this.attachments = task.attachments
   }
 
   get image (): string {
     return this.attachments[0].url
+  }
+
+  get dateDescription (): string {
+    const date = new Date(this.date)
+    const result = format(date, 'D MMMM до hh:mm')
+    return result ?? 'без срока'
   }
 }
